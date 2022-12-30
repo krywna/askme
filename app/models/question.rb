2e33d8765a11139211349d99fa27db8dcbfb4924
krywna <krywnakrywna@gmail.com>
2022-12-30 09:54:39 +0300

    fix regex

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,17 +1,18 @@
 class Question < ApplicationRecord
-  has_many :question_tags
+  has_many :question_tags, dependent: :delete_all
   has_many :tags, through: :question_tags
+
   belongs_to :user
   belongs_to :author, class_name: "User", optional: true
 
   validates :body, presence: true, length: { maximum: 280 }
 
-  before_save :set_tags
+  after_commit :set_tags
 
   def set_tags
     self.tags.clear
-    hashtags = self.body.scan(/#[\wА-Яа-я\-]+/i)
-    hashtags += self.answer.scan(/#[\wА-Яа-я\-]+/i) if self.answer.present?
+    hashtags = self.body.gsub(/#[\p{L}_\d]+/)
+    hashtags += self.answer.gsub(/#[\p{L}_\d]+/) if self.answer.present?
     hashtags.map do |hashtag|
       tag = Tag.find_or_create_by(name: hashtag.downcase.delete("#"))
       self.tags << tag
